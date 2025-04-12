c86af08b16ee38384047686f13df7c5d2eee91dd
nicolenichole <lnicoleu23@gmail.com>
2025-04-11 21:38:12 -0500

    fixing base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/website/',  // Replace with your repository name
   build: {
-    outDir: 'docs'
+    outDir: 'docs'  // or 'dist' if you're deploying that folder, but adjust your GitHub Pages settings accordingly.
   }
 })
+
